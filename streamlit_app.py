5fb0712a2148848a8d6479860d7908cac89f8ef5
aofhafoh <h098s02606@chunchon.gwe.hs.kr>
2025-11-14 15:27:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
        "texts": ["죠타로는 남자", "죠타로는 돌고래", "죠타로의 모자"],
        "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-       "videos": ["https://youtu.be/XXXXXXXXXXX"]
+       "videos": ["https://www.youtube.com/watch?v=nsaai5whYcc"]
      },
 }
 
